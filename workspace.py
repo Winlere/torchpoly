1b9e3bbc7e75375faca140438d6b221fc766779e
winlere <winlere@qq.com>
2024-06-11 07:43:43 -0600

    chore: lockdown workspace

--- workspace.py
+++ workspace.py
@@ -20,39 +20,33 @@ def sanity_check():
     pattern.train(False)
     pattern.weight = nn.Parameter(W)
     pattern.bias = nn.Parameter(b)
+    
+    W = torch.tensor([1, 1, 0, 1], dtype=torch.float32).reshape(2, 2)
+    b = torch.tensor([1, 0], dtype=torch.float32)
+    pattern2 = nn.Linear(2, 2)
+    pattern2.train(False)
+    pattern2.weight = nn.Parameter(W)
+    pattern2.bias = nn.Parameter(b)
+    
     test1 = deeppoly.linear.ArgumentedLinear(pattern)
     test2 = deeppoly.linear.ArgumentedLinear(pattern)
+    test3 = deeppoly.linear.ArgumentedLinear(pattern2)
     relu1 = deeppoly.relu.ArgumentedRelu()
-    layers = [test1, relu1, test2]
+    relu2 = deeppoly.relu.ArgumentedRelu()
+    layers = [test1, relu1, test2, relu2, test3]
 
     lb = [-1, -1]
-    ub = [2, 1]
-    input_info = deeppoly.base.create_input_info(lb, ub)
-    x = input_info.clone()
-
-    for idx, layer in enumerate(layers):
-        x = layer.forward(x)
-        print(f"(Forward) Layer {idx}: {x.lb} {x.ub}")
-        back_info = x.clone()
-
-        # for ridx, rlayer in enumerate(layers[:idx:-1]):
-        # enumerate from idx-1 to 0
-        for ridx, rlayer in enumerate(layers[:idx][::-1]):
-            back_info = rlayer.backsubstitution(back_info)
-            print(
-                f"    (Backsubstitute) Layer {idx - ridx - 1}: {back_info.lb} {back_info.ub}"
-            )
-        if idx != 0:
-            back_info.forward(input_info)
-            layer.update_bounds(x)
-            print(f"    (Update) Layer {idx}: {back_info.lb} {back_info.ub}")
-
-    seq = deeppoly.sequencial.ArgumentedSequential(*layers)
-    x = seq.forward(input_info)
-    print(f"Sequential: {x.lb} {x.ub}")
+    ub = [1, 1]
+    
+    input_x = deeppoly.base.create_input_info(lb=lb, ub=ub, device=torch.device("cuda"))
+    argmodel = deeppoly.sequencial.ArgumentedSequential(*layers)
+    argmodel.cuda()
+    input_x = argmodel.forward(input_x)
+    print(input_x.lb, input_x.ub)
+    
 
 
-def run_exp(precond, model, postcond):
+def run_exp(precond, model, postcond, device=torch.device("cuda")):
     """run an experiment of the model. 
 
     Args:
@@ -67,12 +61,14 @@ def run_exp(precond, model, postcond):
     b = torch.tensor(postcond[1], dtype=torch.float32).reshape(-1)
     
     print(A,b)
-    input_info = deeppoly.base.create_input_info(lb=lb, ub=ub)
+    input_info = deeppoly.base.create_input_info(lb=lb, ub=ub,device=device)
     
     pstcond = deeppoly.postcondition.ArgumentedPostcond(A=A, b=b)
-    argumented_model = deeppoly.sequencial.create_sequencial_from_dirty(model, pstcond)
+    argumented_model = deeppoly.sequencial.create_sequencial_from_dirty(model)
+
     input_info = argumented_model.forward(input_info)
     print(input_info.lb, input_info.ub)
+    # check if input_info is on GPU
     
 
 if __name__ == "__main__":
@@ -96,11 +92,13 @@ if __name__ == "__main__":
     )
     model_path = parser.parse_args().model
     model = parse_onnx.load_onnx(model_path)
+    model = model.eval()
 
     if parser.parse_args().verbose:
         torch.set_printoptions(threshold=1000)
-        print(model)
-        # sanity_check()
+        # print(model)
+        sanity_check()
+        # exit(0)
     
     ast_vnn_node = vnnlib.parse_file(parser.parse_args().vnnlib)
     property_list = vnnlib.compat.CompatTransformer("X", "Y").transform(ast_vnn_node)
