bd052f0e7387da9eb76f331874c4ef97ab04a279
yanglsh <yanglsh@shanghaitech.edu.cn>
2024-06-11 12:45:57 -0600

    chore: clean up

--- torchpoly/cert/__init__.py
+++ torchpoly/cert/__init__.py
@@ -1,5 +1,6 @@
 from .ticket import Ticket
 from .certificate import Certificate, Trace
+from .verifier import Verifier, VerifyResult
 
 
-__all__ = ["Ticket", "Certificate", "Trace"]
+__all__ = ["Ticket", "Certificate", "Trace", "Verifier", "VerifyResult"]
