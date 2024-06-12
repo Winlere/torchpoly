2c2bfad33a5b2291ed61807e83ee5e405adf415e
yanglsh <yanglsh@shanghaitech.edu.cn>
2024-06-12 01:44:26 -0600

    chore: add warning on sequential backward

--- torchpoly/nn/container.py
+++ torchpoly/nn/container.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+import warnings
+
 from typing import Tuple
 
 from torchpoly.cert.certificate import Certificate, Trace
@@ -8,22 +10,27 @@ from torchpoly.nn.module import Module
 
 class SequentialCertificate(Certificate):
 
-    def __init__(self, bound: Ticket, module: nn.Sequential):
+    def __init__(self, bound: Ticket, module: nn.Sequential, device=None):
         super(SequentialCertificate, self).__init__(bound)
         self.module = module
+        self.device = device
 
     def forward(self, state: Tuple[Ticket, Trace]) -> Tuple[Ticket, Trace]:
         for layer in self.module:
-            cert = layer.certify(self.bound)
+            cert = layer.certify(self.bound, self.device)
             state = cert.forward(state)
 
         return state
 
     def backward(self, x: Ticket) -> Ticket:
-        pass
+        warnings.warn(
+            "SequentialCertificate.backward() should not be called directly. "
+            "Please use the backward() method of the certificates returned by the forward() method."
+        )
+        return x
 
 
 class Sequential(Module, nn.Sequential):
 
     def certify(self, bound: Ticket, device=None) -> Certificate:
-        return SequentialCertificate(bound, self)
+        return SequentialCertificate(bound, self, device)
